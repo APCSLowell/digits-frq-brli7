4fc4c600e366ab5ecd559e9ea21201981504a5f6
brli7 <146292564+brli7@users.noreply.github.com>
2025-04-09 11:10:38 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,21 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{  
+	    String sNum = num + "";
+	    for(int i = 0; i < sNum.length(); i++) {
+		digitList.add(Integer.parseInt(sNum.substring(i, i+1)));
+	    }
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	{ 
+		for(int i = 0; i < digitList.size()-1; i++) {
+			if(digitList.get(i) <= digitList.get(i+1)) {
+				return false;
+			}
+		}
+	return true;
 	}
 	
 	public String toString()
